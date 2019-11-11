b139aa10a7251675d9393b91aeb71b9151bcf71c
Vasileios Gkiziakis <gizmux@gmail.com>
2019-11-11 23:07:05 +0000

    match is cool

--- hello-world/file.rs
+++ hello-world/file.rs
@@ -1,16 +1,29 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::io;
+
+
+fn file_to_string(file_name: &str) -> Result<String,io::Error> {
+    let mut file = match File::open(file_name) {
+        Ok(f) => f,
+        Err(e) => return Err(e),
+    };
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => return Ok(contents),
+        Err(e) => return Err(e),
+    }
+} 
 
 fn main() {
     let file_name_arg = env::args().nth(1).expect("Need a filename!");
     let file_name: String = file_name_arg.parse().expect("filename not parsable!");
 
     println!("Opening '{}'...", file_name);
-    let mut file = File::open(file_name).expect("Unable to open file!");
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("couldn't read!");
-
-    println!("dump: {}", contents);
+    match file_to_string(&file_name) {
+        Ok(s) => println!("{}", s),
+        Err(e) => println!("An error happened: {}", e),
+    }
 }
